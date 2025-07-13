04aa1f6e12d549626b3ddd13de74e1f4ebd9fc47
yufu <atashism@gmail.com>
2025-07-13 22:41:59 +0900

    2 yatta

--- src/koans/02_strings.clj
+++ src/koans/02_strings.clj
@@ -37,34 +37,34 @@
  (= "olleh" (string/reverse "hello"))
 
  "Maybe you want to find the index of the first occurrence of a substring"
- (= 0 (string/index-of "hello world" __))
+ (= 0 (string/index-of "hello world" "h"))
 
  "Or maybe the last index of the same substring"
- (= __ (string/last-index-of "hello world, hello" "hello"))
+ (= 13 (string/last-index-of "hello world, hello" "hello"))
 
  "But when something doesn't exist, nothing is found"
- (= __ (string/index-of "hello world" "bob"))
+ (= nil (string/index-of "hello world" "bob"))
 
  "Sometimes you don't want whitespace cluttering the front and back"
- (= __ (string/trim "  \nhello world \t \n"))
+ (= "hello world" (string/trim "  \nhello world \t \n"))
 
  "You can check if something is a char"
- (= __ (char? \c))
+ (= true (char? \c))
 
  "But it may not be"
- (= __ (char? "a"))
+ (= false (char? "a"))
 
  "But chars aren't strings"
- (= __ (string? \b))
+ (= false (string? \b))
 
  "Strings are strings"
- (= true (string? __))
+ (= true (string? "a"))
 
  "Some strings may be blank"
- (= __ (string/blank? ""))
+ (= true (string/blank? ""))
 
  "Even if at first glance they aren't"
- (= __ (string/blank? " \n \t  "))
+ (= true (string/blank? " \n \t  "))
 
  "However, most strings aren't blank"
- (= __ (string/blank? "hello?\nare you out there?")))
+ (= false (string/blank? "hello?\nare you out there?")))
