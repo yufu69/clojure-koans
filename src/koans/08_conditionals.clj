127a133930548e2291bebd561ca0c40d783c67a3
yufu <atashism@gmail.com>
2025-07-29 21:41:53 +0900

    8

--- src/koans/08_conditionals.clj
+++ src/koans/08_conditionals.clj
@@ -9,39 +9,39 @@
         "is that even exercise?"))
 
 (meditations
-  "You will face many decisions"
-  (= __ (if (false? (= 4 5))
-          :a
-          :b))
+ "You will face many decisions"
+ (= :a (if (false? (= 4 5))
+         :a
+         :b))
 
-  "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
-          []))
+ "Some of them leave you no alternative"
+ (= [] (if (> 4 3)
+       []))
 
-  "And in such a situation you may have nothing"
-  (= __ (if (nil? 0)
-          [:a :b :c]))
+ "And in such a situation you may have nothing"
+ (= nil (if (nil? 0)
+         [:a :b :c]))
 
-  "In others your alternative may be interesting"
-  (= :glory (if (not (empty? ()))
-              :doom
-              __))
+ "In others your alternative may be interesting"
+ (= :glory (if (not (empty? ()))
+             :doom
+             :glory))
 
-  "You may have a multitude of possible paths"
-  (let [x 5]
-    (= :your-road (cond (= x __) :road-not-taken
-                        (= x __) :another-road-not-taken
-                        :else __)))
+ "You may have a multitude of possible paths"
+ (let [x 5]
+   (= :your-road (cond (= x 1) :road-not-taken
+                       (= x 2) :another-road-not-taken
+                       :else :your-road)))
 
-  "Or your fate may be sealed"
-  (= 'doom (if-not (zero? __)
-          'doom
-          'more-doom))
+ "Or your fate may be sealed"
+ (= 'doom (if-not (zero? 0)
+            'doom
+            'more-doom))
 
-  "In case of emergency, go fast"
-  (= "pretty fast"
-     (explain-exercise-velocity __))
+ "In case of emergency, go fast"
+ (= "pretty fast"
+    (explain-exercise-velocity :bicycling))
 
-  "But admit it when you don't know what to do"
-  (= __
-     (explain-exercise-velocity :watching-tv)))
+ "But admit it when you don't know what to do"
+ (= "is that even exercise?"
+    (explain-exercise-velocity :watching-tv)))
