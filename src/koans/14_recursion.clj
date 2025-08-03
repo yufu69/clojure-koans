7a36a85b0728f3f94a4f4c9296754caa70cff055
yufu <atashism@gmail.com>
2025-08-03 16:57:51 +0900

    13 - 15

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,21 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (reverse coll))
 
 (defn factorial [n]
-  __)
+  (reduce * (range 1 (inc n))))
 
 (meditations
   "Recursion ends with a base case"
